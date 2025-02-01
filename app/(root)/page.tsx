b0161eac6f6188aa4a13d09c9f5b96f8735d3724
Ayyan Ahmed <ayyanahmed@outlook.com>
2025-02-01 17:43:06 +0500

    Sanity Configuration completed with the card structure on the landing page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,56 @@
-export default function Home() {
+import SearchForm from "@/components/SearchForm";
+import StartupCard from "@/components/StartupCard";
+
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: Promise<{ query?: string }>;
+}) {
+  const query = (await searchParams).query;
+
+  //For Testing Only
+  const posts = [
+    {
+      _createdAt: new Date(),
+      views: 55,
+      author: { _id: 1, name: "Ayyan" },
+      description: "This is a description",
+      image:
+        "https://carelabz.com/wp-content/uploads/2016/02/electricaltesting.jpg",
+      category: "Robots",
+      title: "We Robots",
+    },
+  ];
+
   return (
     <>
-      <h1 className="heading">Home</h1>
+      <section className="pink_container">
+        <h1 className="heading">
+          Pitch Your Ideas, <br /> Connect With Angels
+        </h1>
+
+        <p className="sub-heading !max-w-3xl">
+          Submit Your Vision, Vote on Pitches, and Win the Virtual Competition
+        </p>
+
+        <SearchForm query={query} />
+      </section>
+
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Search results for "${query}"` : "All Startups"}
+        </p>
+
+        <ul className="mt-7 card_grid">
+          {posts?.length > 0 ? (
+            posts.map((post: StartupCardType, index: number) => (
+              <StartupCard key={post?.author._id} post={post} />
+            ))
+          ) : (
+            <p className="no-results">No Startups Found</p>
+          )}
+        </ul>
+      </section>
     </>
   );
 }
